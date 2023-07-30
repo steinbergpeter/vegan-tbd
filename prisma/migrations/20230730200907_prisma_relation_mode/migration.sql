b95ae7375481d4b1d0e94f38d03f285617f70894
Peter Steinberg <bklynpeter@gmail.com>
2023-07-30 16:29:23 -0400

    Add lib files from 'breadit' project
    - add 'auth.ts' and 'db.ts' files from 'breadit' project
    - add '[...nextauth]' and /route.ts files to api/auth/

---
+++ prisma/migrations/20230730200907_prisma_relation_mode/migration.sql
@@ -0,0 +1,11 @@
+-- DropForeignKey
+ALTER TABLE `Account` DROP FOREIGN KEY `Account_userId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `Session` DROP FOREIGN KEY `Session_userId_fkey`;
+
+-- RenameIndex
+ALTER TABLE `Account` RENAME INDEX `Account_userId_fkey` TO `Account_userId_idx`;
+
+-- RenameIndex
+ALTER TABLE `Session` RENAME INDEX `Session_userId_fkey` TO `Session_userId_idx`;
